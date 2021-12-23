629f9c040d469d169d4907fa057fdbc282251f4f
Abhishek Adhikari <abhiayush23@gmail.com>
2021-12-23 12:41:47 +0530

    test changes

--- src/test.js
+++ src/test.js
@@ -4,7 +4,7 @@ let socket = io('http://25.34.155.151:5000');
 
 let roomId = process.argv[0];
 
-socket.emit('subscribe-as-downstream');
+socket.emit('subscribe-as-downstream', roomId);
 
 socket.on('connected', () => {
 	console.log('Connected');
